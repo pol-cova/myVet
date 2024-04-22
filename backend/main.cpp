f288c2a413d5129a9472ebd39a55e1fcde368b1e
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-04-22 08:08:42 -0600

    Add: Homepage

--- backend/main.cpp
+++ backend/main.cpp
@@ -15,6 +15,14 @@ int main()
     return x;
     });
 
+    CROW_ROUTE(app, "/new/visit/petName=<string>&mail=<string>&phone=<string>&date=<string>&hour=<string>").methods(crow::HTTPMethod::GET, crow::HTTPMethod::POST)
+    ([](std::string petName, std::string mail, std::string phone, std::string date, std::string hour){
+     	crow::json::wvalue res({{"petName", petName}, {"mail", mail},
+                               {"phone", phone}, {"date", date}, {"hour", hour}});
+	return res;
+	});
+
+
     //set the port, set the app to run on multiple threads, and run the app
     app.port(18080).multithreaded().run();
 }
